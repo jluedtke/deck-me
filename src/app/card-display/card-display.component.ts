aa63c89eaaff662c7ec8ce6099bb08c29b1abbb9
Jared Luedtke <jared7223@hotmail.com>
2017-07-24 16:46:49 -0700

    start skeleton for removing and couting duplicates

--- src/app/card-display/card-display.component.ts
+++ src/app/card-display/card-display.component.ts
@@ -12,6 +12,9 @@ import { FirebaseToAppService } from '../firebase-to-app.service';
 })
 export class CardDisplayComponent implements OnInit {
   @Input() cardsToDisplay: any[];
+  otherCards: any[];
+  outputCounts: any[];
+
   cardTypes: string[] = ["Creature", "Artifact", "Enchantment", "Instant", "Sorcery", "Planeswalker", "Land"];
   // cardsToDisplay: any[];
 
@@ -19,6 +22,37 @@ export class CardDisplayComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  countRepeats() {
+    this.otherCards = this.cardsToDisplay;
+    this.outputCounts = [];
+    for (let i = 0; i < this.otherCards.length; i++) {
+      let counter: number = 0;
+      for (let ii = 0; ii < this.cardsToDisplay.length; ii++) {
+        if (this.otherCards[i].name == this.cardsToDisplay[ii].name) {
+          counter ++;
+        }
+      }
+      this.outputCounts.push(counter);
+    }
+
+    for (let x = 0; x < this.outputCounts.length; x++) {
+      for (let y = 0; y < this.outputCounts.length; y++) {
+        if (x == y) {
+          continue;
+        } else {
+          if (this.outputCounts[x] == this.outputCounts[y]) {
+            this.outputCounts.splice(y, 1);
+            this.otherCards.splice(y, 1);
+            y--;
+          }
+        }
+      }
+    }
+
+    console.log(this.outputCounts);
+    console.log(this.otherCards);
   }
 
 }
