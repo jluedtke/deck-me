e3f150b03bd5a1be332c8c21f36b9838783b43e5
Adrian Agnic <adrian.agnic@hotmail.com>
2017-07-24 11:55:29 -0700

    changes

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -2,21 +2,17 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable'; //used for user instance
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app'; //access to firebase
+import { User } from './user.model';
 
 @Injectable()
 export class AuthenticationService {
 
   user: Observable<firebase.User>; //current logged-in user
-  current: Object;
+  localUser: Object;
 
   constructor(public afAuth: AngularFireAuth) {
     this.user = afAuth.authState;
   }
-
-  // login() {
-  //   this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
-  //   //target auth prop from NgAuth, and select popup method sending it a google provider
-  // }
 
   createUser(email: string, password: string) {
     this.afAuth.auth.createUserWithEmailAndPassword(email, password).catch(function(error) {
@@ -28,8 +24,7 @@ export class AuthenticationService {
     this.afAuth.auth.signInWithEmailAndPassword(email, password).catch(function(error) {
       alert(error.message);
     })
-    this.current = this.afAuth.auth.currentUser;
-    console.log(this.current[0].S.email);
+    this.localUser = this.afAuth.auth.currentUser;
   }
 
   logout() {
