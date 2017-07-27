b9b3ace678359f7dbbe02964b0f05f8e38098f2d
Adrian Agnic <adrian.agnic@hotmail.com>
2017-07-27 05:14:36 -0700

    add graphs, click update, some error catches, fix var name change in html...thx joe -_-

--- src/app/doughnut-chart/doughnut-chart.component.ts
+++ src/app/doughnut-chart/doughnut-chart.component.ts
@@ -1,21 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { AdriansService } from '../adrians.service';
 
 @Component({
   selector: 'app-doughnut-chart',
-  templateUrl: './doughnut-chart.component.html'
+  templateUrl: './doughnut-chart.component.html',
+  providers: [ AdriansService ]
 })
 export class DoughnutChartComponent {
+  @Input() childCards: any[];
+
+  constructor(public aService: AdriansService) { }
+
   // Doughnut
-  public doughnutChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
-  public doughnutChartData:number[] = [350, 450, 100];
+  public doughnutChartLabels:string[] = ['Red', 'Blue', 'Duals', 'White', 'Green', 'Black'];
+  public doughnutChartData:number[] = [1,1,1,1,1,1];
   public doughnutChartType:string = 'doughnut';
 
   // events
-  public chartClicked(e:any):void {
+  chartClicked(e:any):void {
     console.log(e);
   }
 
-  public chartHovered(e:any):void {
+  chartHovered(e:any):void {
     console.log(e);
+  }
+
+  resetGraph() {
+    this.doughnutChartData = this.aService.getCardColors(this.childCards);
   }
 }
